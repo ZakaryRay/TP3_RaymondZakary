3dd52e78277176415c048069f09b9b401662b615
Zakary Raymond <zakary-raymond@live.fr>
2025-08-20 14:53:49 -0400

    nav est fini

--- js/animation.js
+++ js/animation.js
@@ -18,5 +18,70 @@ window.addEventListener("load", () => {
         }
     );
 });
+let isOpen = false;
 
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+const tl = gsap.timeline();
 
+const animateBtn = () => {
+    tl.clear();
+    tl.to(menuBtn, {
+        opacity: 0,
+        scale: 0.75,
+        duration: 0.3,
+        rotation: "+=90",
+        ease: "power2.inOut",
+    }).to(menuBtn, {
+        opacity: 1,
+        scale: 1,
+        duration: 0.3,
+        ease: "power2.inOut",
+    });
+    tl.play();
+};
+
+menuBtn.addEventListener("click", () => {
+    isOpen = !isOpen;
+    console.log("isOpen:", isOpen);
+
+    if (isOpen) {
+        menuBtn.classList.remove("bx-menu");
+        menuBtn.classList.add("bx-x");
+        gsap.fromTo(
+            navMenu,
+            {
+                opacity: 0,
+                y: -100,
+                scale: 0.5,
+            },
+            {
+                opacity: 1,
+                scale: 1,
+                duration: 0.3,
+                y: 0,
+            }
+        );
+    } else {
+        menuBtn.classList.remove("bx-x");
+        menuBtn.classList.add("bx-menu");
+        gsap.fromTo(
+            navMenu,
+            {
+                opacity: 1,
+                scale: 1,
+
+                y: 0,
+            },
+            {
+                opacity: 0,
+                y: -100,
+                scale: 0.5,
+                duration: 0.3,
+                ease: "back.out",
+            }
+        );
+    }
+
+    animateBtn();
+});
