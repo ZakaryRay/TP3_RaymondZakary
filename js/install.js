3460de2f458d1f657476b242b9b3c7b13c5ea605
Zakary Raymond <zakary-raymond@live.fr>
2025-09-08 21:00:40 -0400

    Ajout du service-worker

---
+++ js/install.js
@@ -0,0 +1,33 @@
+let deferredInstallPrompt = null;
+const installButton = document.getElementById("btnInstall");
+installButton.addEventListener("click", installPWA);
+
+window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+
+function saveBeforeInstallPromptEvent(evt) {
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute("hidden");
+    console.log("oui oui");
+}
+
+function installPWA(evt) {
+    deferredInstallPrompt.prompt();
+
+    evt.srcElement.setAttribute("hidden", true);
+
+    deferredInstallPrompt.userChoice.then((choice) => {
+        if (choice.outcome === "accepted") {
+            console.log("User accepeted the A2HS prompt", choice);
+        } else {
+            console.log("User dismissed the A2HS prompt", choice);
+        }
+
+        deferredInstallPrompt = null;
+    });
+}
+
+window.addEventListener("appinstalled", logAppInstalled);
+
+const logAppInstalled = (evt) => {
+    console.log("L'application Synakode est installé.", evt);
+};
