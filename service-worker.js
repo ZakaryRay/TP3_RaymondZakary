c79300f3291dfe7e2f2475f0b641a9aa2e4ab448
Zakary Raymond <zakary-raymond@live.fr>
2025-09-21 18:55:16 -0400

    Ajustement du site et des problemes du responsive

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,28 @@
-const CACHE_NAME = "static-cache-v9";
+const CACHE_NAME = "static-cache-v10";
 
-const FILES_TO_CACHE = ["offline.html", "air_pump_PNG3.png", "coffin_PNG35.png"];
+const FILES_TO_CACHE = [
+    "offline.html",
+    "animation.js",
+    "accueil.js",
+    "air_pump_PNG3.png",
+    "coffin_PNG35.png",
+    "ak47_PNG15466.png",
+    "audio_cassette_PNG16092.png",
+    "captain_america_PNG91.png",
+    "credit_card_PNG195.png",
+    "electric_scooter_PNG66.png",
+    "grill_PNG13962.png",
+    "leash_PNG134.png",
+    "mince_PNG50.png",
+    "number4_PNG15040.png",
+    "tom_cruise_PNG25.png",
+    "wheelchair_PNG82809.png",
+    "whip_PNG24.png",
+    "whisky_PNG141.png",
+    "LOGO_BLANC.svg",
+    "LOGO_COULEUR.svg",
+    "LOGO_NOIR.svg",
+];
 
 self.addEventListener("install", (evt) => {
     console.log("[ServiceWorker] install");
